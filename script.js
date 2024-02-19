3bcb6d22bc64c14a48fe2b65144d620dfea91cb5
GMzWoods <francois.xdg@gmail.com>
2024-02-19 15:32:56 -0500

    v,1

--- script.js
+++ script.js
@@ -4,41 +4,61 @@ function Jeu(maxEssais) {
     this.maxEssais = 0;
 }
 
+for (let i = 1; i <= 100; i++) {
+    const nombre = document.createElement("div");
+    nombre.classList.add("nombre");
+    nombre.textContent = i;
+    $(".nombres").append(nombre);}
+
 let jeu = new Jeu(10);
 
 function essai() {
     const essai = parseInt($('#essai').val());
-    $('#essais p').text(essai);
-    jeu.nbEssais++;
 
+
+    if (essai > 100 || essai < 1 ){
+        $('#message').text(`Vous devez entrer un chiffre de 1 à 100`).addClass('erreur')
+        Reessayer();
+    }
     if(essai === jeu.nombre) {
-        $('#message').text("Bravo !").addClass('succes');
+        jeu.nbEssais++;
+        $('#message').text("Bravo !").addClass('success');
         fin();
     }
     else {
-        const message = essai < jeu.nombre ? "Trop haut": "Trop bas;";
-        if(jeu.nbEssais > jeu.maxEssais) {
+        const message = essai > jeu.nombre ? "Le nombre est trop haut" : "Le nombre est trop bas;";
+
+        if(jeu.nbEssais === jeu.maxEssais) {
             fin();
         }
+
         else if(essai < jeu.nombre){
-            $('.nombre').filter((index, element)=>parseInt(element.id,10) < jeu.nombre).addClass('cache');
+            jeu.nbEssais++;
+            $('.nombre').filter((index, element)=>parseInt(element.id,1) < jeu.nombre).addClass('cache');
         }
         else {
-            $('.nombre').filter((element)=>parseInt($(element).attr('id'),10) > jeu.nombre).addClass('cache');
+            jeu.nbEssais++;
+            $('.nombre').filter((element)=>parseInt($(element).attr('id'),1) > jeu.nombre).addClass('cache');
         }
-        $('#message').text(`Ah non :(  ${message}`).addClass('erreur');
+        $('#message').text(`Ah non (JO_O)J  ${message}`).addClass('erreur');
     }
+}
+function Reessayer()
+{
+    essai()
 }
 
 function fin(){
-    $('#essais div').addClass('cache');
-    $('#essais').text('<button onclick="nouveauJeu()">Jouer une nouvelle partie</button>');
+
+    $('#essais').html('<button onclick="nouveauJeu()">Jouer une nouvelle partie</button>')
+
+
 }
 
 function nouveauJeu() {
     jeu = new Jeu();
-    $('#essais button').last().remove();
+    $('#essais button').remove();
     $('#essai').val(0);
     $('#essais p').first().text('');
-    $('#message').text().removeClass('succes');
+    $('#message').text().removeClass('success');
 }
